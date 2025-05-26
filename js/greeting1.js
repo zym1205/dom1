ca7d58fbfa95bfd9fb42912296683ff587901ad1
zym1205 <zym20001@qq.com>
2025-05-26 14:44:43 +0800

    js

---
+++ js/greeting1.js
@@ -0,0 +1,10 @@
+
+//默认导出
+export default function sayHello(name){
+    return `你好,${name}!`
+}
+
+//普通导出
+export function sayGoodbye(name) {
+    return `再见, ${name}!`;
+}
---
+++ js/math1.js
@@ -0,0 +1,9 @@
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+export const PI = 3.1415926;
